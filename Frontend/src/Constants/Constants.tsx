c6035e3a696899fd906a9de4d5be7fb38d8a49e5
Hari-Mohan-001 <159766392+Hari-Mohan-001@users.noreply.github.com>
2024-08-23 00:52:55 +0530

    Update Constants.tsx

--- Frontend/src/Constants/Constants.tsx
+++ Frontend/src/Constants/Constants.tsx
@@ -1,6 +1,6 @@
 
-export const URL = 'http://server-sincereedu.sincereedu.ltd/api'
-export const SocketUrL = 'http://server-sincereedu.sincereedu.ltd'
+export const URL = 'https://server-sincereedu.sincereedu.ltd/api'
+export const SocketUrL = 'https://server-sincereedu.sincereedu.ltd'
 
 export const USER_ENDPOINT ="user"
 export const ADMIN_ENDPOINT ="admin"
