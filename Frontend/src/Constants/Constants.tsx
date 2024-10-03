f9dd8233a00ad877942a5426a03bbe9f96fcb5b8
Hari Mohan <hari1111mohan@gmail.com>
2024-10-03 23:50:52 +0530

    changed the urls

--- Frontend/src/Constants/Constants.tsx
+++ Frontend/src/Constants/Constants.tsx
@@ -1,8 +1,8 @@
 
-// export const URL = 'https://server-sincereedu.sincereedu.ltd/api'
-// export const SocketUrL = 'https://server-sincereedu.sincereedu.ltd'
-export const URL = 'http://localhost:3001/api'
-export const SocketUrL = 'http://localhost:3001'
+export const URL = 'https://server-sincereedu.sincereedu.ltd/api'
+export const SocketUrL = 'https://server-sincereedu.sincereedu.ltd'
+// export const URL = 'http://localhost:3001/api'
+// export const SocketUrL = 'http://localhost:3001'
 
 export const USER_ENDPOINT ="user"
 export const ADMIN_ENDPOINT ="admin"
